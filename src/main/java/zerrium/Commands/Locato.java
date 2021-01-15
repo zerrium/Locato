9632a2e730d5500f10c9f84003d80137f988b720
zerrium <zerrium@mail.com>
2021-01-15 21:12:26 +0700

    Locato add command for player done

--- src/main/java/zerrium/Commands/Locato.java
+++ src/main/java/zerrium/Commands/Locato.java
@@ -9,28 +9,31 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import zerrium.SqlCon;
+import zerrium.ZChunk;
 import zerrium.ZLocation;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Locato implements CommandExecutor {
     private CommandSender cs;
+    private final HashMap<String, Location> hm = new HashMap<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         cs = sender;
         final String msg = ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "usage:\n" +
                 "/locato <remove/status> <place_name>\n" +
-                "/locato <add/edit> <place_name> (optional for add: <chunk1 pos x> <chunk1 pos y> <chunk1 pos z> <chunk2 pos x> <chunk2 pos y> <chunk2 pos z> <dimension: NORMAL or NETHER or THE_END>)" +
+                "/locato <add/edit> <place_name> (optional for add and player: <chunk1 pos x> <chunk1 pos y> <chunk1 pos z> <chunk2 pos x> <chunk2 pos y> <chunk2 pos z> <dimension: NORMAL or NETHER or THE_END>)" +
                 "/locato <search> <keyword>";
         switch(args.length){
             case 2:
                 switch(args[0].toLowerCase()){
                     case "add":
-                        doAdd();
+                        doAdd(args[1].toLowerCase());
                         return true;
                     case "remove":
                         doRemove(args[1].toLowerCase());
@@ -150,42 +153,104 @@ public class Locato implements CommandExecutor {
                 if(index == -1){
                     cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "No registered places found with \"" + place + "\" name.");
                 }else{
-                    System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET + " Deleting place: " + place + " from database...");
-                    PreparedStatement pss = null;
-                    Connection con = null;
-                    try {
-                        con = SqlCon.openConnection();
-                        pss = con.prepareStatement("delete from locato where place_id=?");
-                        pss.setString(1, place);
-                        int row = pss.executeUpdate();
-                        System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET +
-                                (row > 0 ? " Deleted place: " + place + " from database." : " No place of " + place + " found on the database. No rows affected."));
-                        zerrium.Locato.zLocations.remove(index);
-                        cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Deleted place: \"" + place + "\" from database record.");
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
-                        cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Failed to delete place: \"" + place + "\" from database record. Check server console for more info.");
-                    }finally {
-                        try {
-                            assert pss != null;
-                            pss.close();
-                            con.close();
-                        } catch (SQLException throwables) {
-                            if(zerrium.Locato.debug) throwables.printStackTrace();
-                        }
-                    }
-
+                    SQL_delete(place, index);
                 }
             }
         };
         r.runTaskAsynchronously(zerrium.Locato.getPlugin(zerrium.Locato.class));
     }
 
-    private void doAdd(){
-
+    private void doAdd(String place){
+        BukkitRunnable r = new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(!(cs instanceof Player)){
+                    cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "usage:\n" +
+                            "/locato <add> <place_name> <chunk1 pos x> <chunk1 pos y> <chunk1 pos z> <chunk2 pos x> <chunk2 pos y> <chunk2 pos z> <dimension: NORMAL or NETHER or THE_END>");
+                    return;
+                }
+                if(!cs.hasPermission("Locato.add")){
+                    cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Sorry you do not have permission to perform this command. Ask your admin for more info.");
+                    return;
+                }
+                if(hm.get(place) == null){
+                    if(zerrium.Locato.zLocations.contains(new ZLocation(place))){
+                        cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Place of \"" + place + "\" is already recorded on database. Try again using another name!");
+                    }else{
+                        hm.put(place, ((Player) cs).getLocation());
+                        cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "First position recorded. Go to second position and perform \"/locato add " + place + "\" again to save this place.");
+                    }
+                }else{
+                    Location lo1 = hm.get(place);
+                    Location lo2 = ((Player) cs).getLocation();
+                    SQL_add(place, lo1.getWorld().getEnvironment().toString(), (int) lo1.getX(), (int) lo1.getZ(), (int) lo1.getY(), (int) lo2.getX(), (int) lo2.getZ(), (int) lo2.getY());
+                }
+            }
+        };
+        r.runTaskAsynchronously(zerrium.Locato.getPlugin(zerrium.Locato.class));
     }
 
     private void doAddEdit(String add_edit){
 
+    }
+
+    private void SQL_add(String place, String dimension, int chunk1_x, int chunk1_z, int elevation1, int chunk2_x, int chunk2_z, int elevation2){
+        System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET + " Adding place: " + place + " to database...");
+        PreparedStatement pss = null;
+        Connection con = null;
+        try {
+            con = SqlCon.openConnection();
+            pss = con.prepareStatement("insert into locato(place_id, dimension, chunk1_x, chunk1_z, elevation1, chunk2_x, chunk2_z, elevation2) values (?, ?, ?, ?, ?, ?, ?, ?");
+            pss.setString(1, place);
+            pss.setString(2, dimension);
+            pss.setInt(3, chunk1_x);
+            pss.setInt(4, chunk1_z);
+            pss.setInt(5, elevation1);
+            pss.setInt(6, chunk2_x);
+            pss.setInt(7, chunk2_z);
+            pss.setInt(8, elevation2);
+            pss.executeUpdate();
+            System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET + " Added place: " + place + " to the database.");
+            zerrium.Locato.zLocations.add(new ZLocation(place, dimension, new ZChunk(chunk1_x, chunk1_z, elevation1), new ZChunk(chunk2_x, chunk2_z, elevation2)));
+            cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Added place: \"" + place + "\" to database record.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Failed to add place: \"" + place + "\" from database record. Check server console for more info.");
+        }finally {
+            try {
+                assert pss != null;
+                pss.close();
+                con.close();
+            } catch (SQLException throwables) {
+                if(zerrium.Locato.debug) throwables.printStackTrace();
+            }
+        }
+    }
+
+    private void SQL_delete(String place, int index){
+        System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET + " Deleting place: " + place + " from database...");
+        PreparedStatement pss = null;
+        Connection con = null;
+        try {
+            con = SqlCon.openConnection();
+            pss = con.prepareStatement("delete from locato where place_id=?");
+            pss.setString(1, place);
+            int row = pss.executeUpdate();
+            System.out.println(ChatColor.YELLOW + "[Locato]" + ChatColor.RESET +
+                    (row > 0 ? " Deleted place: " + place + " from database." : " No place of " + place + " found on the database. No rows affected."));
+            zerrium.Locato.zLocations.remove(index);
+            cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Deleted place: \"" + place + "\" from database record.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            cs.sendMessage(ChatColor.GOLD+"[Locato] " + ChatColor.RESET + "Failed to delete place: \"" + place + "\" from database record. Check server console for more info.");
+        }finally {
+            try {
+                assert pss != null;
+                pss.close();
+                con.close();
+            } catch (SQLException throwables) {
+                if(zerrium.Locato.debug) throwables.printStackTrace();
+            }
+        }
     }
 }
